f7d0f9ab8df1d7f328f9388269ecacfdd8dd24b8
Ahmed Bawazer <40899021+ahmeddeveloper55@users.noreply.github.com>
2023-03-21 20:23:55 +0530

    Delete FronteEnd  directory

--- FronteEnd /AllergyCom-App_Mini-Project/android/app/src/main/kotlin/com/example/minip/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.minip
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- FronteEnd /AllergyCom-App_Mini-Project/lib/Widgets/constants.dart
+++
@@ -1,4 +0,0 @@
-import 'package:flutter/material.dart';
-
-const ActiveColor=Color(0xFF1D1E2);
-const InActiveColor=Color(0xFFFFFFFF);
